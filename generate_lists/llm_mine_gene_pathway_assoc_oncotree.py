a33c44da481f56e13bb3b330faf3e6ee5804ef2a
SuhasiniLulla <suhasinilulla@gmail.com>
2025-08-26 14:05:49 -0500

    addressing issues, updating pathways in llm script, adding .sh run script

--- generate_lists/llm_mine_gene_pathway_assoc_oncotree.py
+++ generate_lists/llm_mine_gene_pathway_assoc_oncotree.py
@@ -9,8 +9,6 @@ from dotenv import load_dotenv
 from google.generativeai.types import GenerationConfig
 from pydantic import BaseModel, ConfigDict, Field
 
-TEMPERATURE = 0.25
-
 load_dotenv()
 YOUR_API_KEY = os.getenv("LLM_API_KEY")
 genai.configure(api_key=YOUR_API_KEY)
@@ -22,46 +20,37 @@ class CodeReferences(BaseModel):
 
 
 class AssociatedPathways(BaseModel):
-    prostate_cancer_ar_signaling: Literal["yes", "no"]
-    prostate_cancer_ar_and_steroid_synthesis_enzymes: Literal["yes", "no"]
-    prostate_cancer_steroid_inactivating_genes: Literal["yes", "no"]
-    prostate_cancer_down_regulated_by_androgen: Literal["yes", "no"]
-    glioblastoma_tp53_pathway: Literal["yes", "no"]
-    glioblastoma_rtk_ras_pi3k_akt_signaling: Literal["yes", "no"]
-    glioblastoma_rb_pathway: Literal["yes", "no"]
-    general_cell_cycle_tcga_pancan_pathways: Literal["yes", "no"]
-    general_hippo_tcga_pancan_pathways: Literal["yes", "no"]
-    general_myc_tcga_pancan_pathways: Literal["yes", "no"]
-    general_notch_tcga_pancan_pathways: Literal["yes", "no"]
-    general_nrf2_tcga_pancan_pathways: Literal["yes", "no"]
-    general_pi3k_tcga_pancan_pathways: Literal["yes", "no"]
-    general_tgf_beta_tcga_pancan_pathways: Literal["yes", "no"]
-    general_rtk_ras_tcga_pancan_pathways: Literal["yes", "no"]
-    general_tp53_tcga_pancan_pathways: Literal["yes", "no"]
-    general_wnt_tcga_pancan_pathways: Literal["yes", "no"]
-    general_cell_cycle_control: Literal["yes", "no"]
-    general_p53_signaling: Literal["yes", "no"]
-    general_notch_signaling: Literal["yes", "no"]
-    general_dna_damage_response: Literal["yes", "no"]
-    general_other_growth_proliferation_signaling: Literal["yes", "no"]
-    general_survival_cell_death_regulation_signaling: Literal["yes", "no"]
-    general_telomere_maintenance: Literal["yes", "no"]
-    general_rtk_signaling_family: Literal["yes", "no"]
-    general_pi3k_akt_mtor_signaling: Literal["yes", "no"]
-    general_ras_raf_mek_erk_jnk_signaling: Literal["yes", "no"]
-    general_angiogenesis: Literal["yes", "no"]
-    general_folate_transport: Literal["yes", "no"]
-    general_invasion_and_metastasis: Literal["yes", "no"]
-    general_tgf_β_pathway: Literal["yes", "no"]
-    ovarian_cancer_oncogenes_associated_with_epithelial_ovarian_cancer: Literal[
-        "yes", "no"
-    ]
-    ovarian_cancer_putative_tumor_suppressor_genes_in_epithelial_ovarian_cancer: (
-        Literal["yes", "no"]
-    )
-    general_regulation_of_ribosomal_protein_synthesis_and_cell_growth: Literal[
-        "yes", "no"
-    ]
+    ar_signaling: Literal["yes", "no"]
+    ar_and_steroid_synthesis_enzymes: Literal["yes", "no"]
+    steroid_inactivating_genes: Literal["yes", "no"]
+    down_regulated_by_androgen: Literal["yes", "no"]
+    rtk_ras_pi3k_akt_signaling: Literal["yes", "no"]
+    rb_pathway: Literal["yes", "no"]
+    cell_cycle_pathway: Literal["yes", "no"]
+    hippo_pathway: Literal["yes", "no"]
+    myc_pathway: Literal["yes", "no"]
+    notch_pathway: Literal["yes", "no"]
+    nrf2_pathway: Literal["yes", "no"]
+    pi3k_pathway: Literal["yes", "no"]
+    rtk_ras_pathway: Literal["yes", "no"]
+    tp53_pathway: Literal["yes", "no"]
+    wnt_pathway: Literal["yes", "no"]
+    cell_cycle_control: Literal["yes", "no"]
+    p53_signaling: Literal["yes", "no"]
+    notch_signaling: Literal["yes", "no"]
+    dna_damage_response: Literal["yes", "no"]
+    other_growth_proliferation_signaling: Literal["yes", "no"]
+    survival_cell_death_regulation_signaling: Literal["yes", "no"]
+    telomere_maintenance: Literal["yes", "no"]
+    rtk_signaling_family: Literal["yes", "no"]
+    pi3k_akt_mtor_signaling: Literal["yes", "no"]
+    ras_raf_mek_erk_jnk_signaling: Literal["yes", "no"]
+    angiogenesis: Literal["yes", "no"]
+    folate_transport: Literal["yes", "no"]
+    invasion_and_metastasis: Literal["yes", "no"]
+    tgf_β_pathway: Literal["yes", "no"]
+    oncogenes_associated_with_epithelial_ovarian_cancer: Literal["yes", "no"]
+    regulation_of_ribosomal_protein_synthesis_and_cell_growth: Literal["yes", "no"]
 
 
 class GeneInfo(BaseModel):
@@ -132,52 +121,12 @@ def generate_gemini_compatible_schema(model: BaseModel) -> Dict[str, Any]:
 
 
 # Auto-generate JSON schema from the Pydantic model
-SCHEMA_JSON = generate_gemini_compatible_schema(GenerateLists)
+schema_json = generate_gemini_compatible_schema(GenerateLists)
 
-print("Generated Schema:\n", json.dumps(SCHEMA_JSON, indent=2))
+print("Generated Schema:\n", json.dumps(schema_json, indent=2))
 
 
-PROMPT_TEMPLATE = """You are an expert in clinical cancer genetics, specifically in gene-disease and pathway-disease curations (for hereditary and sporadic cancers). Based on scientific literature in PubMed, current genetic testing practices in oncology clinics, gene-disease association curations in ClinGen, OMIM, GeneReviews, and similar expert or peer reviewed resoursces,
-and public tumor sequencing databases such as cBioPortal, and COSMIC, list the genes
-and pathways, mutations in which are associated with {cancer_name} ({oncotree_code}).
-Different ontologies have different terms/codes to depict the same cancer sub-type.
-{oncotree_code} is the OncoTree code that is the same as {ncit_code} (NCIt) and {umls_code} (UMLS).
-Use these codes to gather as much literature/data as possible to provide a comprehensive list
-of genes and pathways in JSON structured format. The associated gene list should be ranked by strength and likelihood of association such that the first gene in the list has the strongest association with the cancer type and the last gene in the list has the weakest association with the cancer type. The gene list should be of high quality, accurate, and should not exceed 50 in count. The JSON should have top-level keys:
-"oncotree_code", "cancer_name" (full name of the code), "other_codes_used_for_data_gathering" (dictionary with keys NCIt and UMLS), "associated_genes" (a list of dictionaries - one dictionary for every associated gene, having top level keys of 'gene_symbol' and 'gene_info'. 'gene_info' is a dictionary with keys and values formatted as follows: 1. 'association_strength', value: classified as 'very strong', 'strong', 'moderate', 'weak', or 'very weak' association of this particular gene and cancer type depending on the quality and quantity of resources used to associate the gene and cancer type, 2. 'reference', value: resource(s) used to infer the gene-cancer type association (if multiple citations, then separate instances by '|'), 3. 'mutations', value: list of types of mutations in the gene that is associated with the given cancer type (such as truncating, splice, missense gain of function, missense-loss of function, missense-neomorphic, missense-hypo-/hyper-morphic, deletion, duplication, fusion, copy number variant, structural variant, complex rearrangements, methylation, and so on relevant to the gene-cancer type association), 4. 'mutation_origin', value: MUST be either "germline/somatic" OR "somatic" where 'germline/somatic' indicates that the cancer mutation in this gene can be present in the germline as cancer predisposing or arise somatically over time, 'somatic' indicates that the cancer mutation in this gene is only of somatic origin and not seen in the germline, 5. 'diagnostic_implication', value: clinical implication of the gene as to whether it is used to diagnose the cancer type, for example, the gene KRAS is associated with PAAD: 'diagnostic: missense mutations in KRAS are associated with PAAD and used for diagnosis.' Limit to 1 sentence, 6. 'therapeutic_relevance', value: if gene mutation informs decision making for therapeutic strategy, for example, for the association of KRAS and PAAD, 'clinical trials such as NCT07020221 are actively testing inhibitors of the actionable missense mutation KRAS G12D which is frequent in PAAD. Effect on immunotherapy is ....'), "molecular_subtypes", values: This should be a list of expression-based, genomic, or histological molecular subtypes known to occur in {cancer_name}. These subtypes should be informative for clinical decision-making, such as guiding treatment selection or predicting prognosis. Please use descriptive names or standard nomenclature for the subtypes, and prioritize those with known clinical implications, and "associated_pathways" (a dictionary with keys being each pathway name in the list: ['prostate_cancer_ar_signaling',
- 'prostate_cancer_ar_and_steroid_synthesis_enzymes',
- 'prostate_cancer_steroid_inactivating_genes',
- 'prostate_cancer_down_regulated_by_androgen',
- 'glioblastoma_tp53_pathway',
- 'glioblastoma_rtk_ras_pi3k_akt_signaling',
- 'glioblastoma_rb_pathway',
- 'general_cell_cycle_tcga_pancan_pathways',
- 'general_hippo_tcga_pancan_pathways',
- 'general_myc_tcga_pancan_pathways',
- 'general_notch_tcga_pancan_pathways',
- 'general_nrf2_tcga_pancan_pathways',
- 'general_pi3k_tcga_pancan_pathways',
- 'general_tgf_beta_tcga_pancan_pathways',
- 'general_rtk_ras_tcga_pancan_pathways',
- 'general_tp53_tcga_pancan_pathways',
- 'general_wnt_tcga_pancan_pathways',
- 'general_cell_cycle_control',
- 'general_p53_signaling',
- 'general_notch_signaling',
- 'general_dna_damage_response',
- 'general_other_growth_proliferation_signaling',
- 'general_survival_cell_death_regulation_signaling',
- 'general_telomere_maintenance',
- 'general_rtk_signaling_family',
- 'general_pi3k_akt_mtor_signaling',
- 'general_ras_raf_mek_erk_jnk_signaling',
- 'general_angiogenesis',
- 'general_folate_transport',
- 'general_invasion_and_metastasis',
- 'general_tgf_β_pathway',
- 'ovarian_cancer_oncogenes_associated_with_epithelial_ovarian_cancer',
- 'ovarian_cancer_putative_tumor_suppressor_genes_in_epithelial_ovarian_cancer',
- 'general_regulation_of_ribosomal_protein_synthesis_and_cell_growth'] and the value being 'yes' if associated with cancer sub-type or 'no' if pathway not associated with cancer sub-type)."""
+PROMPT_TEMPLATE = """You are an expert in clinical cancer genetics, specifically in gene-disease and pathway-disease curations (for hereditary and sporadic cancers). Based on scientific literature in PubMed, current genetic testing practices in oncology clinics, gene-disease association curations in ClinGen, OMIM, GeneReviews, and similar expert or peer reviewed resoursces, and public tumor sequencing databases such as cBioPortal, and COSMIC, list the genes and pathways, mutations in which are associated with {cancer_name} ({oncotree_code}). Different ontologies have different terms/codes to depict the same cancer sub-type. {oncotree_code} is the OncoTree code that is the same as {ncit_code} (NCIt) and {umls_code} (UMLS). Use these codes to gather as much literature/data as possible to provide a comprehensive list of genes and pathways in JSON structured format. The associated gene list should be ranked by strength and likelihood of association such that the first gene in the list has the strongest association with the cancer type and the last gene in the list has the weakest association with the cancer type. The gene list should be of high quality, accurate, and should not exceed 50 in count. The JSON should have top-level keys: "oncotree_code", "cancer_name" (full name of the code), "other_codes_used_for_data_gathering" (dictionary with keys NCIt and UMLS), "associated_genes" (a list of dictionaries - one dictionary for every associated gene, having top level keys of 'gene_symbol' and 'gene_info'. 'gene_symbol' should be only 1 gene per key. 'gene_info' is a dictionary with keys and values formatted as follows: 1. 'association_strength', value: classified as 'very strong', 'strong', 'moderate', 'weak', or 'very weak' association of this particular gene and cancer type depending on the quality and quantity of resources used to associate the gene and cancer type, 2. 'reference', value: resource(s) used to infer the gene-cancer type association (if multiple citations, then separate instances by '|'), 3. 'mutations', value: list of types of mutations in the gene that is associated with the given cancer type (such as truncating, splice, missense gain of function, missense-loss of function, missense-neomorphic, missense-hypo-/hyper-morphic, deletion, duplication, fusion, copy number variant, structural variant, complex rearrangements, methylation, and so on relevant to the gene-cancer type association), 4. 'mutation_origin', value: MUST be either "germline/somatic" OR "somatic" where 'germline/somatic' indicates that the cancer mutation in this gene can be present in the germline as cancer predisposing or arise somatically over time (so includes both 'germline' and 'somatic' options in 1 category only), 'somatic' indicates that the cancer mutation in this gene is only of somatic origin and not seen in the germline, 5. 'diagnostic_implication', value: clinical implication of the gene as to whether it is used to diagnose the cancer type, for example, the gene KRAS is associated with PAAD: 'diagnostic: missense mutations in KRAS are associated with PAAD and used for diagnosis.' Limit to 1 sentence, 6. 'therapeutic_relevance', value: if gene mutation informs decision making for therapeutic strategy, for example, for the association of KRAS and PAAD, 'clinical trials such as NCT07020221 are actively testing inhibitors of the actionable missense mutation KRAS G12D which is frequent in PAAD. Effect on immunotherapy is ....'), "molecular_subtypes", values: This should be a list of expression-based, genomic, or histological molecular subtypes known to occur in {cancer_name}. These subtypes should be informative for clinical decision-making, such as guiding treatment selection or predicting prognosis. Please use descriptive names or standard nomenclature for the subtypes, and prioritize those with known clinical implications, and "associated_pathways" (a dictionary with keys being each pathway name in the list: ['ar_signaling', 'ar_and_steroid_synthesis_enzymes', 'steroid_inactivating_genes', 'down_regulated_by_androgen', 'rtk_ras_pi3k_akt_signaling', 'rb_pathway', 'cell_cycle_pathway', 'hippo_pathway', 'myc_pathway', 'notch_pathway', 'nrf2_pathway', 'pi3k_pathway', 'rtk_ras_pathway', 'tp53_pathway', 'wnt_pathway', 'cell_cycle_control', 'p53_signaling', 'notch_signaling', 'dna_damage_response', 'other_growth_proliferation_signaling', 'survival_cell_death_regulation_signaling', 'telomere_maintenance', 'rtk_signaling_family', 'pi3k_akt_mtor_signaling', 'ras_raf_mek_erk_jnk_signaling', 'angiogenesis', 'folate_transport', 'invasion_and_metastasis', 'tgf_β_pathway', 'oncogenes_associated_with_epithelial_ovarian_cancer', 'regulation_of_ribosomal_protein_synthesis_and_cell_growth'] and the value being 'yes' if associated with cancer sub-type or 'no' if pathway not associated with cancer sub-type)."""
 
 
 app = typer.Typer()
@@ -196,6 +145,12 @@ def generate_lists(
         "-model",
         help="enter the string name of the LLM model to be used",
     ),
+    temperature: float = typer.Option(
+        ...,
+        "--input_LLM_temperature",
+        "-temp",
+        help="Temperature setting for LLM: value between 0 to 1",
+    ),
 ):
     typer.echo(f"Input file path: {input_oncotree}")
 
@@ -210,7 +165,7 @@ def generate_lists(
     oncotree_codes_info = {}
 
     for item in oncotree:
-        if item["code"] not in {"COAD", "NSCLC", "PAAD", "DSRCT"}:
+        if item["code"] not in {"COAD", "NSCLC", "PAAD", "DSRCT", "BRCA", "MNM"}:
             continue
         code = item["code"]
         name = item["name"]
@@ -229,9 +184,9 @@ def generate_lists(
         oncotree_codes_info[code] = {"name": name, "NCIt": ncit, "UMLS": umls}
 
     generation_config = GenerationConfig(
-        temperature=TEMPERATURE,
+        temperature=temperature,
         response_mime_type="application/json",  # Ask Gemini to output JSON directly
-        response_schema=SCHEMA_JSON,
+        response_schema=schema_json,
     )
 
     model = genai.GenerativeModel(
