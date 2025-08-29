8797f9e15044247325b9714bd1dbf27b8a86553a
SuhasiniLulla <slulla@bcm.edu>
2025-08-29 11:25:45 -0500

    Update run_llm_generate_lists.sh

--- run_llm_generate_lists.sh
+++ run_llm_generate_lists.sh
@@ -9,7 +9,7 @@ INPUT_FILE="assets/oncotree_latest_stable_June2025.json"
 OUTPUT_FILE="gene_pathway_lists/export_lists_info_6codes.json"
 
 # Run the Python script with the specified arguments
-python generate_lists/llm_mine_gene_pathway_assoc_oncotree.py \
+uv run --active python generate_lists/llm_mine_gene_pathway_assoc_oncotree.py \
     -i "$INPUT_FILE" \
     -o "$OUTPUT_FILE"\
     -model "$MODEL_NAME" \
