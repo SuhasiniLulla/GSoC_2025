9f898bf3e03f2b4a73fe8e5d28a08e03d16ab959
SuhasiniLulla <slulla@bcm.edu>
2025-08-29 11:26:01 -0500

    Update run_validate_gene_pathway_subtype_lists.sh

--- run_validate_gene_pathway_subtype_lists.sh
+++ run_validate_gene_pathway_subtype_lists.sh
@@ -9,7 +9,7 @@ VALIDATION_INPUT_FILE="gene_pathway_lists/export_lists_info_6codes.json"
 REFERENCE_FILE="assets/mmc1.xlsx"
 
 # Run the Python script with the specified arguments
-python generate_lists/validate_genelist.py \
+uv run --active python generate_lists/validate_genelist.py \
     -i "$VALIDATION_INPUT_FILE" \
     -ref "$REFERENCE_FILE" \
     -model "$MODEL_NAME" \
